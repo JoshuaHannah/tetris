78cd283fed5a51014a271972b32dc43ce669214d
JoshuaHannah <joshua.hannah@univ.ox.ac.uk>
2014-10-13 15:22:20 +0100

    ClAdded colour tags to the pieces

--- lib/tetris/pieces.ex
+++ lib/tetris/pieces.ex
@@ -11,40 +11,48 @@ defmodule Tetris.Pieces do
   def new_piece(num) do
     @doc ~S"""
     Uses Erlang's random module to generate a number between 1 and 7, which corresponds to a piece.
+    Exploit the unused position 0 to correspond to the colour of the piece.
     """
     num = :random.uniform(7)
     case num do
       1 -> #line
         HashDict.new
         |> HashDict.put(21, [4,5,6,7])
+        |> HashDict.put(0, :green)
       2 -> #square
         HashDict.new
         |> HashDict.put(22, [5,6])
         |> HashDict.put(21, [5,6])
+        |> HashDict.put(0, :blue)
       3 -> #rL
         HashDict.new
         |> HashDict.put(23, [5])
         |> HashDict.put(22, [5])
         |> HashDict.put(21, [5,6])
+        |> HashDict.put(0, :yellow)
       4 -> #lL
         HashDict.new
         |> HashDict.put(23, [6])
         |> HashDict.put(22, [6])
         |> HashDict.put(21, [5,6])
+        |> HashDict.put(0, :cyan)
       5 -> #rF
        HashDict.new
        |> HashDict.put(23, [5])
        |> HashDict.put(22, [5,6])
        |> HashDict.put(21, [6])
+       |> HashDict.put(0, :red)
       6 -> #lF
         HashDict.new
         |> HashDict.put(23, [6])
         |> HashDict.put(22, [5,6])
         |> HashDict.put(21, [5])
+        |> HashDict.put(0, :white)
       7 -> #middle
        HashDict.new
        |> HashDict.put(22, [5])
        |> HashDict.put(21, [4,5,6])
+       |> HashDict.put(0, :magenta)
     end
   end
 end
